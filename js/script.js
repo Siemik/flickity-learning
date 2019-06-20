02554fed8fa06e612d41f955fe358b4cdc17d241
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-06-20 17:32:37 +0200

    Added vanilla js flickity code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Initialize with vanilla JavaScript
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
