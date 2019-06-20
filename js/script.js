3796b9cdd928458409a18d33bf3ae73b56afdc65
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-06-20 23:11:36 +0200

    Added slider with content

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 // Initialize with vanilla JavaScript
-var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.carousel-main');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true
+});
+var flkty = new Flickity( '.carousel', {
+  hash: true,
+});
+
+var buttonGroup = document.querySelector('.button-group');
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  var selector = event.target.getAttribute('data-selector');
+  flkty.selectCell( selector );
 });
